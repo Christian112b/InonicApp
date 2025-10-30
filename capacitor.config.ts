95caeecb7aa0e52e29b22ceca6b11cf76740f468
Christian Zavala <crizaaron@gmail.com>
2025-10-29 21:41:48 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'costanzoApp',
+  webDir: 'www'
+};
+
+export default config;
